ad991abd23c23a04d97753c77851241d034b8e24
deval-patel <devalrocket@gmail.com>
2021-02-02 21:45:11 -0500

    Add React client code and server directory. Dockerize frontend and backend

---
+++ server/server.js
@@ -0,0 +1,33 @@
+// This is the server file
+import express from "express";
+// import mongoose from "mongoose";
+
+const app = express();
+app.disable("x-powered-by");
+// Body parser
+app.use(express.json());
+
+// Connect to MongoDB
+// mongoose
+// 	.connect(config.get("mongoURI"), {
+// 		useCreateIndex: true,
+// 		useNewUrlParser: true,
+// 		useUnifiedTopology: true,
+// 	})
+// 	.then(() => console.log("MongoDB connected"))
+// 	.catch((err) => console.log(err));
+
+// const db = mongoose.connection;
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
+
+function sleep(ms) {
+	return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+while (1) {
+	await sleep(2000);
+	console.log("pls work bitch");
+}
