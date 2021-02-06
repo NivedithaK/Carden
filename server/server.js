037edad91121f157158b1842f2087302b058dbdb
Niveditha Kani <nivedithakani@gmail.com>
2021-02-05 22:43:59 -0500

    Connect to MongoDB

--- server/server.js
+++ server/server.js
@@ -1,23 +1,30 @@
 // This is the server file
 import express from "express";
-// import mongoose from "mongoose";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+const result = dotenv.config();
+if (result.error) {
+	throw result.error
+}
 
 const app = express();
 app.disable("x-powered-by");
+
 // Body parser
 app.use(express.json());
 
 // Connect to MongoDB
-// mongoose
-// 	.connect(config.get("mongoURI"), {
-// 		useCreateIndex: true,
-// 		useNewUrlParser: true,
-// 		useUnifiedTopology: true,
-// 	})
-// 	.then(() => console.log("MongoDB connected"))
-// 	.catch((err) => console.log(err));
+mongoose
+	.connect(process.env.mongoURI, {
+		useCreateIndex: true,
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	})
+	.then(() => console.log("MongoDB connected"))
+	.catch((err) => console.log(err));
 
-// const db = mongoose.connection;
+const db = mongoose.connection;
 
 const port = process.env.PORT || 5000;
 
@@ -29,5 +36,5 @@ function sleep(ms) {
 
 while (1) {
 	await sleep(2000);
-	console.log("pls work bitch");
+	console.log("pls work");
 }
