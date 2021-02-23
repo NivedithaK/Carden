06477265db69b810b2d3ea2a8957933ddc6b4ead
deval-patel <devalrocket@gmail.com>
2021-02-23 17:34:55 -0500

    Try deploy with docker

--- server/server.js
+++ server/server.js
@@ -65,7 +65,8 @@ app.get("/*", (req, res) => {
 
 const port =
 	process.env.NODE_ENV == "production"
-		? process.env.PORT || 8081
+		? // ? process.env.PORT || 8081
+		  process.env.PORT || 5000
 		: process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
