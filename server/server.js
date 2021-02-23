bd9b1b564de594c9b5e3742770a9f08a445d8590
deval-patel <devalrocket@gmail.com>
2021-02-23 17:20:25 -0500

    Add package-json in root directory

--- server/server.js
+++ server/server.js
@@ -65,7 +65,7 @@ app.get("/*", (req, res) => {
 
 const port =
 	process.env.NODE_ENV == "production"
-		? process.env.PORT || 8080
+		? process.env.PORT || 8081
 		: process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
