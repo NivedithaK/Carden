746aa20b1f46e09766871412b3e29ac9cee5d04c
Emily Wu <chinadragon.emily@gmail.com>
2021-02-23 21:35:09 -0500

    Add initial box setup to Create/index.js

--- client/src/pages/Create/index.js
+++ client/src/pages/Create/index.js
@@ -11,6 +11,9 @@ import {
   FormLabel,
   Input,
   Button,
+  useColorModeValue,
+  Flex,
+  Divider,
 } from "@chakra-ui/react";
 
 function Create() {
@@ -24,9 +27,25 @@ function Create() {
     // if not then history.push("/explore");
   }, []);
   return (
-    <Box w="100%" h="100%" bg="blue.500">
-      <Heading>Creating Template</Heading>
-      {/* {<CreateTemplate props={cardDetails}/> }*/}
+    <Box h="100%" display="flex"> 
+      <Box flex="1"w="100%" bg={useColorModeValue("palette.700")}>
+        <Heading>Creating Template</Heading>
+        {/* {<CreateTemplate props={cardDetails}/> }*/}
+      </Box>
+    
+      <Flex flex="10" direction="row" align="stretch" h="100%" w="100%" > 
+          <Box h="100%" flex="1" bg={useColorModeValue("palette.700")}>
+            Content
+          </Box>
+          <Box h="100%" flex="5" bg={useColorModeValue("palette.600")}>
+            Content
+          </Box>
+          <Box h="100%" flex="1" bg={useColorModeValue("palette.700")}>
+            Content
+          </Box>
+      </Flex>
+      
+      
     </Box>
   );
 }
