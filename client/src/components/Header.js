aae73fbb9e28c899d3bb7745471a660c0e21851b
Syed Raza Abbas <raza.35.abbas@gmail.com>
2021-02-24 19:24:46 -0500

    WEEB-75 Added dark mode capabilites to ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,109 +1,139 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
 import {
-  Box,
-  Heading,
-  Flex,
-  Button,
-  useColorModeValue,
-  Spacer,
-  Switch,
+    Box,
+    Heading,
+    Flex,
+    Button,
+    useColorMode,
+    useColorModeValue,
+    Spacer,
+    Switch,
 } from "@chakra-ui/react";
-import { MoonIcon } from "@chakra-ui/icons";
+import { SunIcon, MoonIcon } from "@chakra-ui/icons";
 
 const MenuItems = ({ children, handleClick }) => {
-  return (
-    <Button
-      variant="outline"
-      borderRadius="20px"
-      mt={{ base: 4, md: 0 }}
-      mr={6}
-      display="block"
-      onClick={handleClick}
-    >
-      {children}
-    </Button>
-  );
+    return (
+        <Button
+            variant="outline"
+            borderRadius="20px"
+            mt={{ base: 4, md: 0 }}
+            mr={6}
+            display="block"
+            onClick={handleClick}
+            borderColor={useColorModeValue("palette.700", "palette.700")}
+            color={useColorModeValue("palette.700", "palette.700")}
+        >
+            {children}
+        </Button>
+    );
 };
 
 const Header = (props) => {
-  const [show, setShow] = React.useState(false);
-  const handleToggle = () => setShow(!show);
-  const history = useHistory();
-  const handleLogin = (e) => {
-    e.preventDefault();
-    history.push("/login");
-  };
-  const handleAbout = (e) => {
-    e.preventDefault();
-    history.push("/");
-  };
-  const handleSignup = (e) => {
-    e.preventDefault();
-    history.push("/signup");
-  };
-  return (
-    <Box>
-      <Flex
-        as="nav"
-        align="center"
-        justify="space-between"
-        wrap="wrap"
-        padding="1.5rem"
-        bg={useColorModeValue("palette.800", "palette.900")}
-        color="white"
-        {...props}
-      >
-        <Flex mr={5}>
-          <Heading size="lg" as="button" onClick={handleAbout}>
-            Carden
-          </Heading>
-        </Flex>
-
-        <Box
-          align="right"
-          display={{ sm: "block", md: "none" }}
-          onClick={handleToggle}
-        >
-          <svg
-            fill="white"
-            width="12px"
-            viewBox="0 0 20 20"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <title>Menu</title>
-            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-          </svg>
-        </Box>
-
-        <Box
-          display={{ sm: show ? "block" : "none", md: "flex" }}
-          width={{ sm: "full", md: "auto" }}
-          alignItems="center"
-          flexGrow={1}
-        >
-          <Spacer />
-          <MenuItems handleClick={handleAbout}>About</MenuItems>
-          <MenuItems handleClick={handleLogin}>Log In</MenuItems>
-          <Box
-            display={{ sm: show ? "block" : "none", md: "block" }}
-            mt={{ base: 4, md: 0 }}
-          >
-            <Button
-              borderRadius="20px"
-              bg={useColorModeValue("palette.200", "palette.900")}
-              color={useColorModeValue("palette.800", "palette.900")}
-              onClick={handleSignup}
+    const [show, setShow] = React.useState(false);
+    const { colorMode, toggleColorMode } = useColorMode();
+    const handleToggle = () => setShow(!show);
+    const history = useHistory();
+    const handleLogin = (e) => {
+        e.preventDefault();
+        history.push("/login");
+    };
+    const handleAbout = (e) => {
+        e.preventDefault();
+        history.push("/");
+    };
+    const handleSignup = (e) => {
+        e.preventDefault();
+        history.push("/signup");
+    };
+    const handleDarkMode = (e) => {
+        // e.preventDefault();
+        toggleColorMode();
+    };
+    return (
+        <Box>
+            <Flex
+                as="nav"
+                align="center"
+                justify="space-between"
+                wrap="wrap"
+                padding="1.5rem"
+                bg={useColorModeValue("palette.800", "paletter.600")}
+                color={useColorModeValue("palette.700", "palette.700")}
+                {...props}
             >
-              Sign Up
-            </Button>
-          </Box>
-          <MoonIcon ml="20px" />
-          <Switch colorScheme="red" size="md" pl="10px" />
+                <Flex mr={5}>
+                    <Heading size="lg" as="button" onClick={handleAbout}>
+                        Carden
+                    </Heading>
+                </Flex>
+
+                <Box
+                    align="right"
+                    display={{ sm: "block", md: "none" }}
+                    onClick={handleToggle}
+                >
+                    <svg
+                        fill="white"
+                        width="12px"
+                        viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg"
+                    >
+                        <title>Menu</title>
+                        <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+                    </svg>
+                </Box>
+
+                <Box
+                    display={{ sm: show ? "block" : "none", md: "flex" }}
+                    width={{ sm: "full", md: "auto" }}
+                    alignItems="center"
+                    flexGrow={1}
+                >
+                    <Spacer />
+                    <MenuItems handleClick={handleAbout}>About</MenuItems>
+                    <MenuItems handleClick={handleLogin}>Log In</MenuItems>
+                    <Box
+                        display={{ sm: show ? "block" : "none", md: "block" }}
+                        mt={{ base: 4, md: 0 }}
+                    >
+                        <Button
+                            borderRadius="20px"
+                            bg={useColorModeValue("palette.200", "palette.200")}
+                            color={useColorModeValue(
+                                "palette.800",
+                                "palette.600"
+                            )}
+                            onClick={handleSignup}
+                            _hover={{
+                                color: useColorModeValue(
+                                    "palette.700",
+                                    "palette.900"
+                                ),
+                                background: useColorModeValue(
+                                    "palette.900",
+                                    "palette.600"
+                                ),
+                            }}
+                        >
+                            Sign Up
+                        </Button>
+                    </Box>
+                    {colorMode === "light" ? (
+                        <MoonIcon ml="20px" />
+                    ) : (
+                        <SunIcon ml="20px" />
+                    )}
+                    <Switch
+                        colorScheme="red"
+                        size="md"
+                        pl="10px"
+                        onChange={handleDarkMode}
+                    />
+                </Box>
+            </Flex>
         </Box>
-      </Flex>
-    </Box>
-  );
+    );
 };
 
 export default Header;
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useHistory } from "react-router-dom";
 import {
-  BrowserRouter as Router,
-  Switch as RRSwitch,
-  Route,
+    BrowserRouter as Router,
+    Switch as RRSwitch,
+    Route,
 } from "react-router-dom";
 import Header from "./components/Header.js";
 import Landing from "./pages/Landing.js";
@@ -16,49 +16,55 @@ import Edit from "./pages/Edit.js";
 import Preview from "./pages/Preview.js";
 import Profile from "./pages/Profile.js";
 import Create from "./pages/Create.js";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import theme from "./theme";
 import { Provider } from "react-redux";
 import { store, persistor } from "./store";
 import { PersistGate } from "redux-persist/integration/react";
 import "./styles/index.css";
 function App() {
-  const history = useHistory();
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        {/* {" "} */}
-        {/* null passed to loading, persistor is being used here */}
-        <Router>
-          <ChakraProvider theme={theme}>
-            <Header />
-            <RRSwitch>
-              <Route exact path="/" component={Landing} />
-              <Route exact path="/signup" component={Signup} />
-              <Route exact path="/login" component={Login} />
-              <Route exact path="/explore" component={Explore} />
-              <Route
-                exact
-                path="/dashboard"
-                component={Dashboard}
-                history={history}
-              />
-              <Route exact path="/edit" component={Edit} />
-              <Route exact path="/preview" component={Preview} />
-              <Route
-                exact
-                path="/profile"
-                component={Profile}
-                history={history}
-              />
-              <Route exact path="/create" component={Create} />
-            </RRSwitch>
-          </ChakraProvider>
-        </Router>
-      </PersistGate>
-    </Provider>
-  );
+    const history = useHistory();
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                {/* {" "} */}
+                {/* null passed to loading, persistor is being used here */}
+                <Router>
+                    <ChakraProvider theme={theme}>
+                        <Header />
+                        <RRSwitch>
+                            <Route exact path="/" component={Landing} />
+                            <Route exact path="/signup" component={Signup} />
+                            <Route exact path="/login" component={Login} />
+                            <Route exact path="/explore" component={Explore} />
+                            <Route
+                                exact
+                                path="/dashboard"
+                                component={Dashboard}
+                                history={history}
+                            />
+                            <Route exact path="/edit" component={Edit} />
+                            <Route exact path="/preview" component={Preview} />
+                            <Route
+                                exact
+                                path="/profile"
+                                component={Profile}
+                                history={history}
+                            />
+                            <Route exact path="/create" component={Create} />
+                        </RRSwitch>
+                    </ChakraProvider>
+                </Router>
+            </PersistGate>
+        </Provider>
+    );
 }
 
 const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+    <>
+        <ColorModeScript initialColorMode={theme.config.initialColorMode} />
+        <App />
+    </>,
+    rootElement
+);
