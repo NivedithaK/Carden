fb27afb5a3f17024c7babbf6f8973430af6919b7
wuemily2 <emil.wu@mail.utoronto.ca>
2021-02-26 00:31:29 -0500

    Add Property menus

--- client/src/pages/Create/index.js
+++ client/src/pages/Create/index.js
@@ -11,6 +11,7 @@ import {
   FormLabel,
   Input,
   Button,
+  ButtonGroup,
   useColorModeValue,
   Flex,
   Divider,
@@ -19,7 +20,14 @@ import {
   NumberInputStepper,
   NumberIncrementStepper,
   NumberDecrementStepper,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  Center,
 } from "@chakra-ui/react";
+
+import { ChevronDownIcon } from '@chakra-ui/icons';
 
 import { SketchPicker } from 'react-color';
 
@@ -99,6 +107,23 @@ function PXStepper({min, max, defaultValue, step}) {
 
 }
 
+function SelectionMenu (props){
+  /**Store Menu Items and Menu Selection in State */
+  const [selection, setSelection] = useState("Select Option");
+  return (
+    <Menu>
+      <MenuButton as={Button} rightIcon={<ChevronDownIcon/> } overflow="hidden">
+        {selection}
+      </MenuButton>
+      <MenuList>
+        <MenuItem onClick={()=> setSelection("Select Option")}>Select Option</MenuItem>
+        <MenuItem onClick={()=> setSelection("Placeholder1")}>Placeholder1</MenuItem>
+        <MenuItem onClick={()=> setSelection("Placeholder2")}>Placeholder2</MenuItem>
+      </MenuList>
+    </Menu>
+  )
+}
+
 function ColorSelector({changeColor, props}){
   const [color, setColor] = React.useState({r: 191, g:251, b:255, a: 1});
   const [displayPicker, showDisplayPicker] = React.useState(false);
@@ -142,6 +167,10 @@ function Create() {
     // if not then history.push("/explore");
   }, []);
   const [canvasColor, setCanvasColor] = useState({r: 191, g:251, b:255, a: 1});
+  const properties = {default: 1, text: 2, img: 3, button: 4}
+  const [propertyMenu, setPropertyMenu] = useState(properties.default);
+
+  //Todo = elements list of things on the canvas
 
   // Color selector can change this thing's color <Box bg={`rgba(${canvasColor.r}, ${canvasColor.g}, ${canvasColor.b}, ${canvasColor.a})`}>Placeholder</Box>
 
@@ -159,22 +188,22 @@ function Create() {
         bg={useColorModeValue("palette.600")}
          > 
          {/**Left Sidebar */}
-          <Flex h="100%" flex="1" zIndex={3} direction={{sm: "row", lg: "column"}} bg={useColorModeValue("palette.700")}>
+          <Flex h="100%" flex="2" zIndex={3} direction={{sm: "row", lg: "column"}} bg={useColorModeValue("palette.700")}>
 
             {/**Canvas Attributes */}
-            <ToolSection  flex="1" title="Canvas Attributes" >
+            <ToolSection  flex="1" title="Canvas Attributes">
               <Heading size="sm" padding={1}>Dimensions</Heading>
               <Divider/>
-              <ToolItem flex="1" m="auto" label='Width'> 
+              <ToolItem  m="auto" label='Width'> 
                 <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
               </ToolItem>
-              <ToolItem flex="1" m="auto" label='Height'> 
+              <ToolItem  m="auto" label='Height'> 
                 <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
               </ToolItem>
 
               <Heading size="sm" padding={1}>Background Color</Heading>
               <Divider/>
-              <ToolItem  flex="1" m="auto" label="Select">
+              <ToolItem   m="auto" label="Select">
                   <ColorSelector changeColor={setCanvasColor}/>
               </ToolItem>
             </ToolSection>
@@ -185,39 +214,125 @@ function Create() {
               {/**Below are placeholder buttons that do not have actual drag and drop functionality */}
               <Divider/>
               <Flex h="70%" direction={{sm: "column", md: "row", lg: "column"}}>
-                <DragAndDropItem m="1em" flex="1" label="Add Text"></DragAndDropItem>
-                <DragAndDropItem m="1em" flex="1" label="Add Image"></DragAndDropItem>
-                <DragAndDropItem m="1em" flex="1" label="Add Button"></DragAndDropItem>
+                {/**The placeholder buttons should only bring up the property list right now */}
+                <DragAndDropItem m="1em" flex="1" label="Add Text" onClick={() => setPropertyMenu(properties.text)}></DragAndDropItem>
+                <DragAndDropItem m="1em" flex="1" label="Add Image" onClick={() => setPropertyMenu(properties.img)}></DragAndDropItem>
+                <DragAndDropItem m="1em" flex="1" label="Add Button" onClick={() => setPropertyMenu(properties.button)}></DragAndDropItem>
               </Flex>
             </ToolSection>
           </Flex>
           {/**
            * Center box
            */}
-          <Box h="100%" flex="5" zIndex={1} bg={useColorModeValue("palette.600")} overflow="hidden">
-
-          <Box 
-              
-              bg={`rgba(${canvasColor.r}, ${canvasColor.g}, ${canvasColor.b}, ${canvasColor.a})`}
-              zIndex={2}
-              //Centering
-              pos="relative"
-              top="50%"
-              left="50%"
-              transform="translate(-50%, -50%)"
-              // Todo: add the ablity to resize the canvas and reposition within the user's view. Potentially need another box
-              w="1000px"
-              h="600px"
-              maxH="100%"
-              maxW="100%"
-              >
-                Placeholder Canvas
+          <Box h="100%" flex="9" zIndex={1} bg={useColorModeValue("palette.600")} overflow="hidden">
+            <Box 
+                bg={`rgba(${canvasColor.r}, ${canvasColor.g}, ${canvasColor.b}, ${canvasColor.a})`}
+                zIndex={2} //This should appear under any menu item but above the grey space
+                //Centering
+                pos="relative"
+                top="50%"
+                left="50%"
+                transform="translate(-50%, -50%)"
+                // Todo: add the ablity to resize the canvas and reposition within the user's view. Potentially need another box
+                w="1000px"
+                h="600px"
+                maxH="100%"
+                maxW="100%"
+                >
+                  Placeholder Canvas
             </Box>
-            
           </Box>
           {/**Right Sidebar */}
-          <Flex h="100%" flex="1" zIndex={3} bg={useColorModeValue("palette.700") }>
-            {/**Todo: dynamically relocate and resize the canvas or something */}
+          <Flex h="100%" w="100%" flex="2" zIndex={3} bg={useColorModeValue("palette.700") }>
+            {/**Properties of the currently selected item with conditional rendering */}
+            <ToolSection title="Properties" w="100%">
+              <Divider/>
+              <ToolItem label="Component" m="auto" textAlign="center"> 
+                {propertyMenu == properties.default ? "No Component Selected" : null}
+                {propertyMenu == properties.text ? "Text" : null}
+                {propertyMenu == properties.img ? "Image" : null}
+                {propertyMenu == properties.button ? "Button" : null}
+              </ToolItem>
+              <Divider/>
+
+              {propertyMenu == properties.text || propertyMenu == properties.button ?
+                <Box>
+                    <ToolItem label="Font">
+                      <SelectionMenu/>
+                    </ToolItem>
+                    <ToolItem label="Font-Size">
+                      <PXStepper min={8} max={144} defaultValue={12} step={2}/>
+                    </ToolItem>
+                    <ToolItem label="Color">
+                      <ColorSelector changeColor={(placeholder) => {}}/>
+                    </ToolItem>
+                    <Center>
+                      <Heading size="sm" padding={1}>Style</Heading>
+                    </Center>
+                    <Center w="100%">
+                      <ButtonGroup size="sm" isAttached variant="outline">
+                        <Button>Bold</Button>
+                        <Button>Italics</Button>
+                        <Button>Underline</Button>
+                      </ButtonGroup>
+                    </Center>
+                </Box> : null
+              }
+              {propertyMenu == properties.text ?
+                <Box>
+                    <Center>
+                      <Heading size="sm" padding={1}>Text Alignment</Heading>
+                    </Center>
+                    <Center w="100%">
+                      <ButtonGroup size="sm" isAttached variant="outline">
+                        <Button>Left</Button>
+                        <Button>Center</Button>
+                        <Button>Right</Button>
+                      </ButtonGroup>
+                    </Center>
+                </Box> : null
+              }
+
+              {propertyMenu != properties.default ? 
+                <Box>
+                  <ToolItem label="x-position">
+                  <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
+                  </ToolItem>
+                  <ToolItem label="y-position">
+                    <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
+                  </ToolItem>
+                  <ToolItem label="box-width">
+                    <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
+                  </ToolItem>
+                  <ToolItem label="box-height">
+                    <PXStepper min={100} max={2000} defaultValue={500} step={10}/>
+                  </ToolItem>
+                  <ToolItem label="box-color">
+                    <ColorSelector changeColor={(placeholder) => {}}/>
+                  </ToolItem>
+                  <ToolItem label="Animation">
+                    <SelectionMenu/>
+                  </ToolItem>
+                </Box> :null
+              }
+              
+              {propertyMenu == properties.button ?
+                <Box>
+                  <ToolItem label="Links to">
+                    <SelectionMenu/>
+                  </ToolItem>
+                  <ToolItem label="Transition">
+                    <SelectionMenu/>
+                  </ToolItem>
+                </Box>
+                :null
+              }
+
+              
+
+            </ToolSection>
+
+            
             
           </Flex>
       </Flex>
