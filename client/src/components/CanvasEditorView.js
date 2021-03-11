937f73a041fdba89ba89c913a551c62561346c1d
wuemily2 <emil.wu@mail.utoronto.ca>
2021-03-11 17:43:21 -0500

    Fix canvas color setting hook

--- client/src/components/CanvasEditorView.js
+++ client/src/components/CanvasEditorView.js
@@ -21,7 +21,7 @@ class CanvasEditorView extends Component{
             <CanvasEditorHeader flex="6" w="100%" zIndex={5}/>
             <ThickHDivider flex="0.3" colorstring={"palette.800"}/>
             <CanvasEditorBottom flex="93.7"
-                colorColorSetter={(color) => this.setState({canvasColor: color})} 
+                canvasColorSetter={(color) => this.setState({canvasColor: color})} 
                 canvasWidthHook={(width)=> this.setState({canvasWidth: width})}
                 canvasHeightHook={(height)=> this.setState({canvasHeight: height})}
                 canvasColor = {`rgba(${this.state.canvasColor.r}, ${this.state.canvasColor.g}, ${this.state.canvasColor.b}, ${this.state.canvasColor.a})`}
