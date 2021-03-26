2988987227e9586eb877714e556236733b2dba3c
Niveditha Kani <nivedithakani@gmail.com>
2021-03-25 23:19:31 -0400

    Add sorting endpoints

--- server/routes/api/templates.js
+++ server/routes/api/templates.js
@@ -9,9 +9,9 @@ const router = express.Router();
  * @access      public
  */
 router.get("/", async (req, res) => {
-  await Template.find()
-    .then((templates) => res.json(templates))
-    .catch((e) => res.status(500).json({ error: e.message }));
+	await Template.find()
+		.then((templates) => res.json(templates))
+		.catch((e) => res.status(500).json({ error: e.message }));
 });
 
 /**
@@ -20,20 +20,76 @@ router.get("/", async (req, res) => {
  * @access      public
  */
 router.get("/:id", async (req, res) => {
-  await Template.findById(req.params.id)
-    .then((template) => res.json(template))
-    .catch((e) => res.status(404).json({ error: "template does not exist" }));
+	await Template.findById(req.params.id)
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
+});
+
+/**
+ * @route       GET api/templates/alphabetical
+ * @description Get a list of templates in alphabetical order
+ * @access      public
+ */
+router.get("/tag/alphabetical", async (req, res) => {
+	await Template.find()
+		.sort({ title: 1 })
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
+});
+
+/**
+ * @route       GET api/templates/popular
+ * @description Get a list of templates by number of favourites
+ * @access      public
+ */
+router.get("/tag/popular", async (req, res) => {
+	await Template.find()
+		.sort({ stars: 1 })
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
+});
+
+/**
+ * @route       GET api/templates/newest
+ * @description Get a list of templates, newest first
+ * @access      public
+ */
+router.get("/tag/newest", async (req, res) => {
+	await Template.find({}, null, { sort: "-postDate" })
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
+});
+
+/**
+ * @route       GET api/templates/oldest
+ * @description Get a list of templates, oldest first
+ * @access      public
+ */
+router.get("/tag/oldest", async (req, res) => {
+	await Template.find({}, null, { sort: "postDate" })
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
 });
 
 /**
  * @route       POST api/templates
- * @description Add a template to the database
+ * @description Find template by partial title match
  * @access      public
  */
 router.post("/", async (req, res) => {
-  await Template.create(req.body)
-    .then((template) => res.json(template))
-    .catch((e) => res.status(500).json({ error: e.message }));
+	await Template.create(req.body)
+		.then((template) => res.json(template))
+		.catch((e) => res.status(500).json({ error: e.message }));
 });
 
 /**
@@ -42,9 +98,11 @@ router.post("/", async (req, res) => {
  * @access      public
  */
 router.put("/:id", async (req, res) => {
-  Template.findByIdAndUpdate(req.params.id, req.body, { new: true })
-    .then((template) => res.json(template))
-    .catch((e) => res.status(404).json({ error: "template does not exist" }));
+	Template.findByIdAndUpdate(req.params.id, req.body, { new: true })
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
 });
 
 /**
@@ -53,9 +111,11 @@ router.put("/:id", async (req, res) => {
  * @access      public
  */
 router.delete("/:id", async (req, res) => {
-  await Template.findByIdAndRemove(req.params.id)
-    .then((template) => res.json(template))
-    .catch((e) => res.status(404).json({ error: "template does not exist" }));
+	await Template.findByIdAndRemove(req.params.id)
+		.then((template) => res.json(template))
+		.catch((e) =>
+			res.status(404).json({ error: "template does not exist" })
+		);
 });
 
 export default router;
