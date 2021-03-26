866c660dad016c6ee597f547b272e02f28a7160b
Georges Hanna <georges.hanna@mail.utoronto.ca>
2021-03-26 04:49:44 -0400

    Button uploads to database and showcases the image/gif.

---
+++ Serverless/lambdas/endpoints/imageList.js
@@ -0,0 +1,33 @@
+import * as AWS from 'aws-sdk';
+
+const s3 = new AWS.S3();
+
+exports.handler = async (event, context) => {
+
+    // console.log('Received event:', JSON.stringify(event, null, 2));
+
+    var params = { 
+        Bucket: process.env.imageUploadBucket
+    }
+
+    console.log("Checkpoint 1");
+
+    let s3Objects
+
+    try {
+       s3Objects = await s3.listObjectsV2(params).promise();
+       console.log(s3Objects)
+    } catch (e) {
+       console.log(e)
+    }
+
+
+    console.log("Checkpoint 2");
+
+    // Assuming you're using API Gateway
+    return {
+        statusCode: 200,
+        body: JSON.stringify(s3Objects || {message: 'No objects found in s3 bucket'})
+    }
+
+};
